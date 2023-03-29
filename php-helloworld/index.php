28d64b692a3cbfe2791e7c135e557d0b8b834895
GTPE Student <lab-user@bastion.h2vkp.internal>
2023-03-29 16:40:25 +0000

    Make change with image

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,4 +1,12 @@
-<?php
-print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "Opensshit + Prisma is perfect solution !";
-?>
+<style>
+.background {
+  background-image: url('https://assets.openshift.com/hubfs/Imported_Blog_Media/Logotype_RH_OpenShiftContainerPlatform_wLogo_CMYK_Black.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+</style>
+<body>
+  <div class="background">
+    <!-- your content here -->
+  </div>
+</body>
