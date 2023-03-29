859c1a476054c0e2bec203467e1450a444ca46e8
GTPE Student <lab-user@bastion.h2vkp.internal>
2023-03-29 17:31:19 +0000

    Make a change

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -26,9 +26,8 @@ h2 {
 <title>Welcome To OpenShift & Prisma Cloud</title>
 </head>
 <body>
-<h1>     WELCOM TO EVENT </h1>
+<h1>     WELCOME TO EVENT </h1>
 <h1> OPENSHIFT & PRISMA CLOUD </h1>
 <!--<h2> THIS IS PERFECT SOLUTION. LET'S TRY !!! </h2> -->
-<h2> THIS IS PERFECT SOLUTION. LET'S TRY !!! </h2>
 </body>
 </html>
